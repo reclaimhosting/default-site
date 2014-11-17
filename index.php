d38d0d15b155e76bd8a97c16b5ebec4ea8d648d6
root <root@saints.reclaimhosting.com>
2014-11-16 23:57:22 -0500

    add the files

---
+++ index.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html>
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<title>Reclaim Hosting</title>
+	<link rel="Shortcut Icon" href="/favicon.ico" type="image/x-icon" />
+	<meta http-equiv="pragma" content="no-cache" />
+	<meta name="robots" content="all" />
+	<meta name="MSSmartTagsPreventParsing" content="true" />
+	<meta http-equiv="imagetoolbar" content="false" />	
+	<style>
+		* { padding: 0; margin: 0; }
+		p { color: #4b4b4b; font-family: Helvetica; font-size: 16px; line-height: 24px; margin-bottom: 22px; -webkit-font-smoothing: antialiased; }
+		a { color: #000; font-weight: bold; text-decoration: none; }
+		a:hover { color: #549eb9; }
+		#monogram { width: 50px; padding: 20px 0 0 0; margin: 0 auto 0 50px; }
+		#about { width: 360px; padding: 0 0 0 150px; margin: -35px 50px 0 auto !important; }
+	</style>
+	<script type="text/javascript"> var domain = document.location.host;</script>
+</head>	
+    
+<body>
+
+<!-- swap out for a logo -->
+<div id="monogram">
+	<img src="logo.png" />
+</div>
+
+<!-- swap out for information about yourself -->
+<div id="about">
+	<p>Welcome to Reclaim Hosting! Your domain, <strong><script type="text/javascript">document.write(domain);</script></strong>, is successfully hosted and working. You can <a href="https://portal.reclaimhosting.com/clientarea.php">log in to your control panel</a> to get started configuring your domain, and if you have any problems please feel free to <a href="mailto:support@reclaimhosting.com">contact us</a>.</p> 
+</div>
+
+<!--include jquery & backstretch-->
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+<script type="text/javascript" src="jquery.backstretch.min.js"></script>
+<script type="text/javascript">
+	$.backstretch("bg.jpg", {speed: 150});
+</script>
+
+</body>	
+</html>
